2ed4d18927e1641224e05094b28d98cc0a4bae15
kk1590 <zhangke_outlook@outlook.com>
2018-10-18 16:24:51 +0800

    第一次提交

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("abc")
+    public String method01(){
+        return "abc";
+    }
+}
